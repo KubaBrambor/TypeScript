2fef2a6b9a39127337206c5d6fa20071083dfae0
KubaBrambor <kuba.brambor@gmail.com>
2021-01-11 12:42:33 +0100

    adding tests for addQuantity method in products-list component

--- ecommerce/src/app/main/products-list/products-list.component.spec.ts
+++ ecommerce/src/app/main/products-list/products-list.component.spec.ts
@@ -1,5 +1,6 @@
 import { ProductsListComponent } from './products-list.component';
 import { Product } from '../../model/product';
+import { ProductsComponent } from '../products/products.component';
 
 describe('AppComponent', () => {
   it('should have product instantiated on ngInit', () => {
@@ -10,9 +11,19 @@ describe('AppComponent', () => {
       new Product('Stol', 20, 'url', false, 20));
   });
 
-  it('should add product', () => {
+  it('should add product to favourite', () => {
     const productListComponent = new ProductsListComponent();
     productListComponent.ngOnInit();
-    expect(productListComponent.productsList[0].favourite).toBeFalsy()
+    expect(productListComponent.productsList[0].favourite).toBeFalsy();
+    productListComponent.productsList[0].favourite = true;
+    expect(productListComponent.productsList[0].favourite).toBeTruthy();
+  })
+
+  it('should add amount', () => {
+    const productListComponent = new ProductsListComponent();
+    productListComponent.ngOnInit();
+    expect(productListComponent.productsList[1].amount).toEqual(1);
+    productListComponent.addQuantity(1);
+    expect(productListComponent.productsList[1].amount).toEqual(2);
   })
 })
