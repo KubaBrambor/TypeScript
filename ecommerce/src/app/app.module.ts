26462c9e7f0733b41dfcf1db8ed97269fba0fcc4
Jakub Brambor <kuba.brambor@gmail.com>
2021-01-15 20:23:16 +0100

    learning forms in Angular

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './main/products/products.component';
 import { ProductsListComponent } from './main/products-list/products-list.component';
+import { CreateProductComponent } from './main/create-product/create-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ecommerce/src/app/main/create-product/create-product.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../model/product';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+export class CreateProductComponent {
+  public product: Product; 
+  
+  constructor() {
+    this.product = new Product("Margaryna", 20, 'url', false, 20);
+  }
+
+}
