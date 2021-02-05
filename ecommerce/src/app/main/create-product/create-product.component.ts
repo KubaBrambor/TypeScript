ca95365173bbdce47aa40c041420673b34cf2986
KubaBrambor <kuba.brambor@gmail.com>
2021-02-05 21:36:46 +0100

    creating controls from component

--- ecommerce/src/app/main/create-product/create-product.component.ts
+++ ecommerce/src/app/main/create-product/create-product.component.ts
@@ -10,6 +10,7 @@ export class CreateProductComponent {
   public product: Product; 
   public productArr: {}[] = [];
   public confirmed: boolean = false;
+  public imageUrlValidator: string;
   
   constructor() {
     this.product = new Product("", 0, '', false, 0);
@@ -19,11 +20,15 @@ export class CreateProductComponent {
     this.product.name = event.toUpperCase()
   }
   createProduct(productForm) {
+    console.log(productForm)
     if(productForm.valid){
       this.productArr.push({});
       Object.assign(this.productArr[this.productArr.length-1], this.product)
       console.log(this.productArr);
     } else { 
+      if(productForm.form.controls.productURL.touched){
+        this.imageUrlValidator = "This field is mandatory! Please paste image URL."
+      }
       console.error("Stock form is in an invalid state.")
     }
     
