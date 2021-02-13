a3cf8ec3e0f150ddc7630ec943f39c4dac18a475
KubaBrambor <kuba.brambor@gmail.com>
2021-02-13 20:20:50 +0100

    adding disable button option if form is not valid

--- ecommerce/src/app/main/register/register.component.ts
+++ ecommerce/src/app/main/register/register.component.ts
@@ -8,6 +8,7 @@ import { User } from '../../model/user';
 })
 export class RegisterComponent implements OnInit {
   public selected:string;
+  public nameEmptyInput:string;
   constructor() {
     this.selected = "male";
    }
@@ -16,8 +17,26 @@ export class RegisterComponent implements OnInit {
   }
 
   createUser(registerForm){
+    console.log(registerForm)
     if(registerForm.valid){
-      console.log(registerForm)
+      const name = registerForm.form.controls.registerUser.controls.userName.value;
+      const surname = registerForm.form.controls.registerUser.controls.userSurname.value;
+      const age = registerForm.form.controls.registerUser.controls.userAge.value;
+      const sex = registerForm.form.controls.registerUser.controls.userSex.value;
+      const street = registerForm.form.controls.registerUser.controls.userStreet.value;
+      const postal_code = registerForm.form.controls.registerUser.controls.userPostalCode.value;
+      const city = registerForm.form.controls.registerUser.controls.userCity.value;
+      const adress = {
+        street:street,
+        postal_code:postal_code,
+        city:city
+      }
+
+      console.log(new User(name, surname, age, sex, adress));
+    } else {
+      // if(registerForm.form.controls.registerUser.controls.userName.value === ""){
+      //   this.nameEmptyInput = ""
+      // }
     }
   }
 
