4cb4b46eb7ac9cc7605e0246571695226c4db5dd
KubaBrambor <kuba.brambor@gmail.com>
2021-03-05 18:50:54 +0100

    adding formControl to registerRX component

--- ecommerce/src/app/main/register-rx/register-rx.component.ts
+++ ecommerce/src/app/main/register-rx/register-rx.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-rx',
   templateUrl: './register-rx.component.html',
   styleUrls: ['./register-rx.component.css']
 })
-export class RegisterRXComponent implements OnInit {
-
+export class RegisterRXComponent {
+  public nameControl = new FormControl();
   constructor() { }
 
-  ngOnInit() {
+  onSubmit() {
+    console.log('Name COntrol Value', this.nameControl.value)
   }
-
 }
