4141f51b125657ed9e9acef09f69fadb39adabd2
KubaBrambor <kuba.brambor@gmail.com>
2021-03-07 19:34:11 +0100

    adding alerts to postal code field

--- ecommerce/src/app/main/register-rx/register-rx.component.ts
+++ ecommerce/src/app/main/register-rx/register-rx.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-register-rx',
@@ -7,31 +7,41 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register-rx.component.css']
 })
 export class RegisterRXComponent {
-  public registerForm: FormGroup = new FormGroup({
-    nameControl: new FormControl(null, Validators.required),
-    surnameControl: new FormControl(null, Validators.required),
-    emailControl: new FormControl(null, [
-      Validators.required,
-      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
-    ]),
-    ageControl: new FormControl(null, Validators.required),
-    genderControl: new FormControl('male'),
-    streetControl: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(3)
-    ]),
-    postalCodeControl: new FormControl(null, [
-      Validators.required,
-      Validators.pattern("^\\d{2}[- ]{0,1}\\d{3}$")
-    ]),
-    cityControl: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(2)
-    ])
-  })
-  constructor() { }
+  public registerForm: FormGroup; 
+  public formBuilder: FormBuilder;
+  constructor(private fb: FormBuilder) {
+   }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+   createForm() {
+     this.registerForm = this.fb.group({
+        nameControl: [null, Validators.required],
+        surnameControl: [null, Validators.required],
+        emailControl: [null, [
+          Validators.required,
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+        ]],
+        ageControl: [null, Validators.required],
+        genderControl: ['male'],
+        streetControl: [null, [
+          Validators.required,
+          Validators.minLength(3)
+        ]],
+        postalCodeControl: [null, [
+          Validators.required,
+          Validators.pattern("^\\d{2}[- ]{0,1}\\d{3}$")
+        ]],
+        cityControl: [null, [
+          Validators.required,
+          Validators.minLength(2)
+        ]]
+      });
+   };
 
   onSubmit() {
-    console.log('Name COntrol Value', this.registerForm.value)
+    console.log('Name COntrol Value', this.registerForm.value, this.registerForm)
   }
 }
