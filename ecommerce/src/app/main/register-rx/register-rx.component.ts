7dc6ada744b64ecb922e2873d4a97eecb11f9ae7
KubaBrambor <kuba.brambor@gmail.com>
2021-03-07 16:04:35 +0100

    creating register form

--- ecommerce/src/app/main/register-rx/register-rx.component.ts
+++ ecommerce/src/app/main/register-rx/register-rx.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-rx',
@@ -7,10 +7,31 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./register-rx.component.css']
 })
 export class RegisterRXComponent {
-  public nameControl = new FormControl();
+  public registerForm: FormGroup = new FormGroup({
+    nameControl: new FormControl(null, Validators.required),
+    surnameControl: new FormControl(null, Validators.required),
+    emailControl: new FormControl(null, [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+    ]),
+    ageControl: new FormControl(null, Validators.required),
+    genderControl: new FormControl('male'),
+    streetControl: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    postalCodeControl: new FormControl(null, [
+      Validators.required,
+      Validators.pattern("^\\d{2}[- ]{0,1}\\d{3}$")
+    ]),
+    cityControl: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(2)
+    ])
+  })
   constructor() { }
 
   onSubmit() {
-    console.log('Name COntrol Value', this.nameControl.value)
+    console.log('Name COntrol Value', this.registerForm.value)
   }
 }
