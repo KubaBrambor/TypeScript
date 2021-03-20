67d3193b15f5dfae7f0b18739554c8bc445b97c1
KubaBrambor <kuba.brambor@gmail.com>
2021-03-20 13:22:53 +0100

    adding validators and getters

--- ecommerce/src/app/main/register-rx/register-rx.component.ts
+++ ecommerce/src/app/main/register-rx/register-rx.component.ts
@@ -18,13 +18,17 @@ export class RegisterRXComponent {
 
    createForm() {
      this.registerForm = this.fb.group({
-        nameControl: [null, Validators.required],
-        surnameControl: [null, Validators.required],
+        nameControl: [null, [Validators.required, Validators.minLength(2)]],
+        surnameControl: [null, [Validators.required, Validators.minLength(2)]],
         emailControl: [null, [
           Validators.required,
           Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
         ]],
-        ageControl: [null, Validators.required],
+        ageControl: [null, [
+          Validators.required, 
+          Validators.min(1),
+          Validators.pattern("^[0-9]*$")
+        ]],
         genderControl: ['male'],
         streetControl: [null, [
           Validators.required,
@@ -40,6 +44,11 @@ export class RegisterRXComponent {
         ]]
       });
    };
+  get name() { return this.registerForm.get('nameControl') };
+  get surname() { return this.registerForm.get('surnameControl') };
+  get postalCode() { return this.registerForm.get('postalCodeControl') };
+  get email() { return this.registerForm.get('emailControl') };
+  get age() { return this.registerForm.get('ageControl') };
 
   onSubmit() {
     console.log('Name COntrol Value', this.registerForm.value, this.registerForm)
